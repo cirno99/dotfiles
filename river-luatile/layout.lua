137c046e585bee02e20f27b35d7d6bf21ee3a7ed
cirno99 <1563800293@qq.com>
2023-12-16 00:44:16 +0800

    add bspwm layout

--- river-luatile/layout.lua
+++ river-luatile/layout.lua
@@ -8,11 +8,11 @@ local utils = require("utils")
 
 OUTER_GAPS = 3
 INNER_GAPS = 3
-MAIN_COUNT = 2
+MAIN_COUNT = 1
 MAIN_RATIO = 0.65
 REVERSE = false
 PREFER_HORIZONTAL = true
-SMART_GAP = true
+SMART_GAPS = true
 
 
 local overrides = {}
@@ -34,7 +34,8 @@ local function get_tag_state(args)
 		return overriden
 	end
 	local default = {
-		{ name = "tile", symbol = "[]=", gaps = 3, main_ratio = 0.60 },
+		-- { name = "tile", symbol = "[]=", gaps = 3, main_ratio = 0.60 },
+		{ name = "bspwm", symbol = "[]+" },
 		{ name = "stack", symbol = "[[]" },
 		{ name = "centered", symbol = "[[c]]" },
 	}
@@ -82,6 +83,111 @@ local function stack(args, layout)
 	return retval
 end
 
+local function bspwm(args, layout)
+	local revert = function(retval)
+		return require("utils").revert(args, retval)
+	end
+	--
+
+	local retval = {}
+	if args.count == 1 then
+		if SMART_GAPS then
+			table.insert(retval, { 0, 0, args.width, args.height })
+		else
+			table.insert(retval, { OUTER_GAPS, OUTER_GAPS, args.width - OUTER_GAPS * 2, args.height - OUTER_GAPS * 2 })
+		end
+	elseif args.count > 1 then
+		local main_w = (args.width - OUTER_GAPS * 2 - INNER_GAPS) * MAIN_RATIO
+		local main_h = args.height - OUTER_GAPS * 2
+		table.insert(retval, {
+			OUTER_GAPS,
+			OUTER_GAPS,
+			main_w,
+			main_h,
+		})
+		-- we are doing a dwindle layout here. first window is the biggest, the rest are smaller
+		for i = 2, args.count do
+			local w, h, x, y
+			local isEven = i % 2 == 0
+			local isOdd = not isEven
+			-- width:
+			-- if it is even, its width is previous window's width * (1/ratio - 1)
+			-- if it is odd, its width depends on:
+			-- it is the last: width = previous window's width
+			-- it is not the last: width = (previous window's width - gaps ) * ratio
+			if isEven then
+				w = retval[i - 1][3] * (1 / MAIN_RATIO - 1)
+			else
+				if i == args.count then
+					w = retval[i - 1][3]
+				else
+					w = (retval[i - 1][3] - INNER_GAPS) * MAIN_RATIO
+				end
+			end
+
+			-- height:
+			-- if is the second window (i == 2) it depends on:
+			-- it is the last window: h = previous window's height
+			-- it is not the last window: h = (main_h - 3 * gaps) * ratio
+			-- otherwise:
+			-- if it is odd, h = previous window's height * (1/ratio - 1)
+			-- if it is even, it depends on:
+			-- it is the last: h = previous window's height
+			-- it is not the last: h = (previous window's height - gaps) * ratio
+			if i == 2 then
+				if i == args.count then
+					h = retval[i - 1][4]
+				else
+					h = (args.height - 2 * OUTER_GAPS - INNER_GAPS) * MAIN_RATIO
+				end
+			else
+				if isOdd then
+					h = retval[i - 1][4] * (1 / MAIN_RATIO - 1)
+				else
+					if i == args.count then
+						h = retval[i - 1][4]
+					else
+						h = (retval[i - 1][4] - INNER_GAPS) * MAIN_RATIO
+					end
+				end
+			end
+
+			-- x:
+			-- if it is even, x = previous window's x + previous window's width + gaps
+			-- if it is odd, x = previous window's x
+			if isEven then
+				x = retval[i - 1][1] + retval[i - 1][3] + INNER_GAPS
+			else
+				x = retval[i - 1][1]
+			end
+
+			-- y:
+			-- if it is even, y = previous window's y
+			-- if it is odd, y = previous window's y + previous window's height + gaps
+			if isEven then
+				y = retval[i - 1][2]
+			else
+				y = retval[i - 1][2] + retval[i - 1][4] + INNER_GAPS
+			end
+
+			table.insert(retval, {
+				x,
+				y,
+				w,
+				h,
+			})
+		end
+	end
+
+	if REVERSE then
+		retval = revert(retval)
+	end
+
+	return retval
+
+
+end
+
 local function centered(args, layout)
 	local retval = {}
 
@@ -100,7 +206,7 @@ local function centered(args, layout)
 	-- Let N be the number of windows
 	-- N = 1 and N = 2 are special cases
 	if args.count == 1 then
-		if SMART_GAP then
+		if SMART_GAPS then
 			table.insert(retval, { 0, 0, args.width, args.height })
 		else
 			table.insert(retval, { OUTER_GAPS, OUTER_GAPS, args.width - OUTER_GAPS * 2, args.height - OUTER_GAPS * 2 })
@@ -220,6 +326,8 @@ function handle_layout(args)
 		return stack(args, layout)
 	elseif layout.name == "centered" then
 		return centered(args, layout)
+	elseif layout.name == "bspwm" then
+		return bspwm(args, layout)
 	end
 end
 
